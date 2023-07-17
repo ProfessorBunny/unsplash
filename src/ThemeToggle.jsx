c2c653118749670826c7b8160cd17fe338d2ab47
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-17 13:29:08 +0100

    Initial setup fro dark theme

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,6 +1,17 @@
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import { useGlobalContext } from "./context";
 const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
   return (
-    <h2>ThemeToggle</h2>
-  )
-}
-export default ThemeToggle
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillSunFill className="toggle-icon" />
+        ) : (
+          <BsFillMoonFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
+};
+export default ThemeToggle;
--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,15 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleDarkTheme = () => {
+    setIsDarkTheme((prevState) => !prevState);
+  };
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => useContext(AppContext);
