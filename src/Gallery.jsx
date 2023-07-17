fd4f7f4f83c285ef6658666c4b296057652f76bc
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-17 12:25:52 +0100

    Context clean up

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,8 +1,6 @@
 import { useGlobalContext } from "./context";
 
 const Gallery = () => {
-  const { test } = useGlobalContext();
-  console.log(test);
   return <h2>Gallery</h2>;
 };
 export default Gallery;
--- src/context.jsx
+++ src/context.jsx
@@ -3,8 +3,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const test = "testing context";
-  return <AppContext.Provider value={{ test }}>{children}</AppContext.Provider>;
+  return <AppContext.Provider>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(AppContext);
