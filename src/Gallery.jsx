7ea1e135ff3da5ee7a01918ebdc7c8032de8a518
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-20 11:32:41 +0100

    Images rendered on the site

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,58 @@
+import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "./context";
-
+import axios from "axios";
+const url =
+  "https://api.unsplash.com/search/photos?client_id=rbzaN_8QdHWEy_qK7RV3heqKTVRf7yzkFYu9ZG5Kp7M&query=cat";
 const Gallery = () => {
-  return <h2></h2>;
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h3>Sorry!</h3>
+        <h4>Can't proces your request at the moment :(</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+  if (response.data.results.length < 1) {
+    return (
+      <section className="image-container">
+        <h3>No result Found!</h3>
+        <h4>Try a different value</h4>
+      </section>
+    );
+  }
+  console.log(results);
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.full;
+        return (
+          <img
+            src={url}
+            alt={item.alt_description}
+            key={item.id}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
