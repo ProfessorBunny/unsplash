a67e312636306d7f208a85b3d2e5d24623c30dd0
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-20 12:47:29 +0100

    set the default site theme based on browser theme

--- src/context.jsx
+++ src/context.jsx
@@ -3,19 +3,32 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext({
   isDarkTheme: false,
   toggleDarkTheme: () => {},
-  searchValue: "football",
+  searchValue: "football  ",
   setSearchValue: () => {},
 });
 
+const getBrowserSetMode = () => {
+  const defaultBrowserMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  console.log(defaultBrowserMode);
+  return defaultBrowserMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getBrowserSetMode());
   const [searchValue, setSearchValue] = useState("football");
   const toggleDarkTheme = () => {
-    const newDarkTheme = !isDarkTheme;
-    setIsDarkTheme(newDarkTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newDarkTheme);
+    setIsDarkTheme((prevstate) => {
+      return !prevstate;
+    });
   };
+  useEffect(() => {
+    // const body = document.querySelector("body");
+    // body.classList.toggle("dark-theme", isDarkTheme);
+    //Same thing as above
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
   return (
     <AppContext.Provider
       value={{ isDarkTheme, toggleDarkTheme, searchValue, setSearchValue }}
