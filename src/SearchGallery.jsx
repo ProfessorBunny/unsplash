d44b42ca59098f30a92b0702974695992965f7a5
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-20 10:41:42 +0100

    Search form added

--- src/SearchGallery.jsx
+++ src/SearchGallery.jsx
@@ -1,7 +1,25 @@
 const SearchGallery = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
   return (
     <section>
       <h1 className="title">Unsplash</h1>
+      <form className="search-form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="Football"
+        />
+        <button type="submit" className="btn">
+          {" "}
+          Search{" "}
+        </button>
+      </form>
     </section>
   );
 };
